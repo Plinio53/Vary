ecc6510ba23542d04b2a10a8e6f7ebfc3ac9e277
Diogo <43014908+Diogo912@users.noreply.github.com>
2019-06-22 20:28:30 +0100

    OwO agora dá para ver o emoji mencionado!

---
+++ commands/EmojiCommand.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+
+exports.run = async (vary, message, args) => {
+    const embed = new Discord.RichEmbed()
+    .setImage(emoji.url)
+message.reply(embed)
+}
+
+exports.config = {
+    name: 'emoji',
+    aliases: ['emote']
+}
