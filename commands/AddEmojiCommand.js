59997be2719197aa19632fd292aa56d15f12fdb4
Diogo <43014908+Diogo912@users.noreply.github.com>
2019-06-22 19:57:52 +0100

    Update AddEmojiCommand.js

--- commands/AddEmojiCommand.js
+++ commands/AddEmojiCommand.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
 
-exports.run = async ({ vary, message, args }, t) => {
+exports.run = async (vary, message, args) => {
 if (!message.member.hasPermission("MANAGE_EMOJIS")) return message.reply("Permissão `Gerenciar Emojis` não encontrada em seu cargo!!")
   message.guild.createEmoji(args[0], args[1])
   message.reply('Emoji criado com sucesso!')
