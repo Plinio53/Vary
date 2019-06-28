bdc4a4f6a59f8f1efb4d54c57f90e1abff05b623
Diogo <43014908+Diogo912@users.noreply.github.com>
2019-06-28 14:48:26 +0100

    Atualizar PayPal de donations que fica nos stats

--- events/ready.js
+++ events/ready.js
@@ -10,7 +10,7 @@ module.exports = async (vary) => {
       {name: '🎶 música', type: 'LISTENING'},
       {name: `🎬 videos`, type: 'WATCHING'},
       {name: `😜 meu website: https://vary.website/`, type: 'WATCHING'},
-      {name: `😢 + 💶 = 😋😘 (https://paypal.me/Diogo912C)`, type: 'PLAYING'},
+      {name: `😢 + 💶 = 😋😘 (https://paypal.me/maidVary)`, type: 'PLAYING'},
      {name: `Fui desenvolvido por ${vary.users.get('375627393773207554').tag}`, type: 'STREAMING', url: 'https://twitch.tv/imdiogo912'},
     {name: `🐦 Me siga no Twitter: @maidVary`, type: 'WATCHING'}
 
