47cee65c96c189b8d8794f4e4c2f31a561e502d1
Diogo912 <gameshakers910@gmail.com>
2019-07-06 10:53:22 +0100

    testar locales en-us no comando de play

--- commands/PlayCommand.js
+++ commands/PlayCommand.js
@@ -8,12 +8,12 @@ exports.run = async ({vary, message, args}, t) => {
 
   if (vary.lavalinkManager.manager.has(message.guild.id)) {
     vary.calls.get(message.guild.id).play(args.join(' ')).then(info => {
-      message.reply(`\`${info.title}\` adicionado à fila`)
+      message.reply(t("commands:play.addQueue", {track: info.title}))
     })
   } else {
     let player = await vary.lavalinkManager.join(message.member.voiceChannel.id)
     player.on('playingNow', track => {
-      message.channel.send(`Tocando agora: ${track.info.title}`)
+      message.channel.send(t("commands:play.playingNow", {track: track.info.title}))
       vary.calls.get(message.guild.id).playingNow = track
     })
     player.play(args.join(' '))
