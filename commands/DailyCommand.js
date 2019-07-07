2dc0de99950bc7d15306eab883c1e0406da85b44
Diogo <43014908+Diogo912@users.noreply.github.com>
2019-07-07 12:45:20 +0100

    Update DailyCommand.js

--- commands/DailyCommand.js
+++ commands/DailyCommand.js
@@ -3,12 +3,22 @@ const mongoose = require("mongoose")
 const db = require("../database.js")
 
 exports.run = ({vary, message, args}, t) => {
+var money = [
+    "350",
+    "750",
+    "137",
+    "1096",
+    "12",
+    "6",
+    "2395"
+];
+let daily = (replys1[Math.floor(Math.random() * daily.length)])
   db.Users.findOne({userID: message.author.id}, (err, user) =>{
       if (user) {
           if ((86400000 / 2) - (Date.now() - user.coinsLastTime) > 0) return message.channel.send('Você já pegou seus Smiles diários hoje, espere para pegar novamente')
-          user.Smiles += 350
+          user.Smiles += daily
           user.coinsLastTime = Date.now()
-          message.reply('Você pegou 350 smiles diários')
+          message.reply('Você pegou ${daily} smiles diários')
           user.save();
           ;
 } else {
