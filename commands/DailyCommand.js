ced0a339e497a65301063592d278d59ec08a7ad4
Pedro <38635037+MysteryMS@users.noreply.github.com>
2019-07-09 15:01:37 -0300

    Update DailyCommand.js

--- commands/DailyCommand.js
+++ commands/DailyCommand.js
@@ -6,7 +6,7 @@ exports.run = ({vary, message, args}, t) => {
   db.Users.findOne({userID: message.author.id}, (err, user) =>{
       if (user) {
           if ((86400000 / 2) - (Date.now() - user.coinsLastTime) > 0) return message.channel.send('Você já pegou seus Smiles diários hoje, espere para pegar novamente')
-          user.Smiles += 350
+          user.Smiles = user.Smiles + 350
           user.coinsLastTime = Date.now()
           message.reply('Você pegou 350 smiles diários')
           user.save();
