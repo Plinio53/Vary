0c272b91d299087459b6a603eaf7c4ecdcd8fd9f
Diogo <43014908+Diogo912@users.noreply.github.com>
2019-07-17 13:56:58 +0100

    embeds em json

--- commands/SayCommand.js
+++ commands/SayCommand.js
@@ -3,9 +3,12 @@ const Discord = require("discord.js");
 exports.run = async ({vary, message, args}, t) => {
 if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Permissão `Gerenciar mensagens` não encontrada em seu cargo!")
 if (args[0] == null) {return message.channel.send(`Digite algo para eu mandar!`)}
-const sayeMessage = args.join(" ");
     message.delete().catch(O_o=>{});
-    message.channel.send(sayeMessage);
+    try {
+JSON.parse(args.join(" "))
+} catch (e) {
+message.channel.send(args.join(" "))
+}
   }
 
   exports.config = {
