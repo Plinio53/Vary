33a87dc47ab70cdae7231c8ef28bd18a3ed66be2
Diogo912 <gameshakers910@gmail.com>
2019-07-22 17:31:19 +0100

    fix

--- commands/PauseCommand.js
+++ commands/PauseCommand.js
@@ -3,9 +3,9 @@ const Discord = require("discord.js");
 exports.run = ({vary, message, args}, t) => {
 
   let player = vary.calls.get(message.guild.id)
-  if (!player.player.track) message.channel.send('Não estou tocando nada no momento!')
+  if (!player.player.track) message.channel.send('Não estou tocando nada no momento.')
   else {
-  player.pause().then(message.channel.send('Música pausada!'))
+  message.channel.send('Música pausada!').then(player.pause())
   }
 }
 
--- commands/SkipCommand.js
+++ commands/SkipCommand.js
@@ -2,11 +2,11 @@ const Discord = require("discord.js");
 
 exports.run = ({vary, message, args}, t) => {
 
-  let player = vary.calls.get(message.guild.id)
-  if (!player.player.track) message.channel.send('Não estou tocando nada no momento!')
-  else {
-  player.stop().then(message.channel.send('Música pulada!'))
-  }
+let player = vary.calls.get(message.guild.id)
+if (!player.player.track) message.channel.send('Não estou tocando nada no momento.')
+else {
+message.channel.send('Música pulada!').then(player.skip())
+}
 }
 
 exports.config = {
