17ee72704376082932fe92d0e7b8cae43e7021ab
Diogo912 <gameshakers910@gmail.com>
2019-07-24 12:46:43 +0100

    update serverinfo

--- commands/ServerInfoCommand.js
+++ commands/ServerInfoCommand.js
@@ -9,27 +9,30 @@ emojis = 'Nenhum';
 emojis = message.channel.guild.emojis.map(e => e).join(" ");
 }
 */
-let online = message.guild.members.filter(member => member.user.presence.status !== 'offline');
-let day = message.guild.createdAt.getDate()
-let month = 1 + message.guild.createdAt.getMonth()
-let year = message.guild.createdAt.getFullYear()
- let sicon = message.guild.iconURL;
+
+let guild = message.guild.id || vary.guilds.get(args[0])
+
+let online = guild.members.filter(member => member.user.presence.status !== 'offline');
+let day = guild.createdAt.getDate()
+let month = 1 + guild.createdAt.getMonth()
+let year = guild.createdAt.getFullYear()
+ let sicon = guild.iconURL;
  let serverembed = new Discord.RichEmbed()
- .setAuthor(message.guild.name, sicon)
+ .setAuthor(guild.name, sicon)
  .setFooter(`📅 Server criado • ${day}.${month}.${year}`)
  .setColor("#7289DA")
  .setThumbnail(sicon)
- .addField("<:discordcanary:601043488619888690> Nome", message.guild.name, true)
- .addField("<:appleid:533758440892268584> ID", message.guild.id, true)
- .addField("<:coroinha:519546425080807434> Dono", message.guild.owner.user.tag, true)
- .addField("🌎 Região", message.guild.region, true)
- .addField("💬 Canais", message.guild.channels.size, true)
- .addField("<:usuarios_azul:572140873601646633> Membros", message.guild.memberCount, true)
- .addField("🙋 Humanos", message.guild.memberCount - message.guild.members.filter(m => m.user.bot).size, true)
- .addField("<:Blobbot:575059789848248340> Bots", message.guild.members.filter(m => m.user.bot).size, true)
+ .addField("<:discordcanary:601043488619888690> Nome", guild.name, true)
+ .addField("<:appleid:533758440892268584> ID", guild.id, true)
+ .addField("<:coroinha:519546425080807434> Dono", guild.owner.user.tag, true)
+ .addField("🌎 Região", guild.region, true)
+ .addField("💬 Canais", guild.channels.size, true)
+ .addField("<:usuarios_azul:572140873601646633> Membros", guild.memberCount, true)
+ .addField("🙋 Humanos", guild.memberCount - guild.members.filter(m => m.user.bot).size, true)
+ .addField("<:Blobbot:575059789848248340> Bots", guild.members.filter(m => m.user.bot).size, true)
  .addField("<:online:601044843736727600> Online", online.size, true)
 // .addField("Emojis", emojis, true)
- .addField("💼 Cargos", message.guild.roles.size, true);
+ .addField("💼 Cargos", guild.roles.size, true);
  message.channel.send(serverembed);
 
 }
