73d6d8822be9021d22515eea0d5120ac8a2304cd
Diogo912 <gameshakers910@gmail.com>
2019-07-24 19:54:34 +0100

    página para mudar o saldo do user

--- website.js
+++ website.js
@@ -1,19 +1,15 @@
 require('dotenv').config()
 const express = require("express")
-
 const request = require("request")
-
 const bt = require("btoa")
-
 const {redirect_uri,id} = require("./config.json")
 const secret = process.env.SECRET
-
 const app = express()
+const database = require("./database.js")
+const authorized = ["375627393773207554"]
 
 app.set('trust proxy', 1) 
-
 const session = require("express-session")
-
 app.use(session({ secret: 'keyboard cat', cookie: { }, resave: false, saveUninitialized: true,}))
 
 
@@ -79,6 +75,25 @@ app.get("/login",async function (req, res) {
        })
 
    })
+
+app.get("/changemoney",function(req,res){
+    if (!req.session.user) return res.redirect("/")
+    if (!authorized.includes(req.session.user.id)) return res.redirect("/")
+    res.render("changemoney.ejs",{
+      username: req.session.user.username
+    })
+})
+app.post("/changemoney",async function(req,res){
+    const {user,money} = req.body
+    if ( (!user || !money) && money !== 0 ) return res.end("Inválido")
+    if (!req.session.user) return res.end("Não autorizado")
+    if (!authorized.includes(req.session.user.id)) return res.end("Não autorizado")
+    const data = await database.Users.findOne({"userID":user})
+    if (!data) return res.end("Não achei esse usuário no banco de dados")
+    data.money = money
+    data.save()
+    return res.end(`Dinheiro do usuário ${user} mudado para ${money} com sucesso`)
+})
 
 })
 
