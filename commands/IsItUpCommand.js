806fcdec52a11eeb2d5554ab3e1b1ac5288c8691
Diogo912 <gameshakers910@gmail.com>
2019-07-24 13:38:48 +0100

    fix isitup

--- commands/IsItUpCommand.js
+++ commands/IsItUpCommand.js
@@ -3,15 +3,14 @@ const fetch = require('node-fetch')
 
 exports.run = async ({vary, message, args}, t) => {
     url = url.replace(PROTOCOL_REGEX, '').replace(PATH_REGEX, '')
-    const embed = new SwitchbladeEmbed(author)
-    channel.startTyping()
+    const embed = new Discord.RichEmbed()
     const body = await fetch(`https://isitup.org/${url}.json`).then(res => res.json())
     if (body.response_code) {
       body.response_time *= 1000
       embed.setTitle(t("commands:isitup.isUp"))
         .setDescription(t("commands:isitup.details", { body }))
     } else {
-      throw new CommandError(t("commands:isitup.isDown"))
+      message.reply(t("commands:isitup.isDown"))
     }
     message.channel.send(embed)
 }
