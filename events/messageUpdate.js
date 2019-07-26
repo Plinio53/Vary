71555bbbc2b75a6ce1f03c325cb39cccea1cf58c
Diogo <43014908+Diogo912@users.noreply.github.com>
2019-07-26 09:02:26 +0100

    fix

--- events/messageUpdate.js
+++
@@ -1,7 +0,0 @@
-const Discord = require("discord.js")
-
-module.exports = async (vary, message, oldMessage, newMessage) => {
-    if (oldMessage === newMessage) return
-    vary.emit("message", newMessage)
-
-  }  
