0b788df27b065286ef118e2cff3a7c0942243e11
Diogo912 <gameshakers910@gmail.com>
2019-07-26 08:45:20 +0100

    change client > vary

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -2,6 +2,6 @@ const Discord = require("discord.js")
 
 module.exports = async (vary, message, args) => {
     if (oldMessage === newMessage) return
-    client.emit("message", newMessage)
+    vary.emit("message", newMessage)
 
   }  
