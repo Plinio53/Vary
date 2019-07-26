7d72dc8fdba45b77acdac74fd74f5f808005bf40
Diogo912 <gameshakers910@gmail.com>
2019-07-26 08:44:55 +0100

    messageUpdate event

---
+++ events/messageUpdate.js
@@ -0,0 +1,7 @@
+const Discord = require("discord.js")
+
+module.exports = async (vary, message, args) => {
+    if (oldMessage === newMessage) return
+    client.emit("message", newMessage)
+
+  }  
