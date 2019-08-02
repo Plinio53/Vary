e5d124a204ed95da577b5ca1111457aa4fc95ca2
Diogo <43014908+ItsDiogo912@users.noreply.github.com>
2019-08-02 18:32:52 +0100

    fix stop

--- commands/StopCommand.js
+++ commands/StopCommand.js
@@ -1,12 +1,12 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 
-exports.run = ({vary, message, args}, t) => {
+exports.run async = ({vary, message, args}, t) => {
 
 let player = vary.calls.get(message.guild.id)
 if (!player.player.track) message.channel.send('Não estou tocando nada no momento.')
 else {
-message.channel.send('Música terminada!').then(player.stop())
-}
+message.channel.send('Música terminada!').then(player.player.stop())
+ }
 }
 
 exports.config = {
