c66eb7bfd5cfa56a9927accf3ce42ee5fa237abf
Diogo912 <gameshakers910@gmail.com>
2019-08-02 18:18:22 +0100

    stop command

---
+++ commands/StopCommand.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+exports.run = ({vary, message, args}, t) => {
+
+let player = vary.calls.get(message.guild.id)
+if (!player.player.track) message.channel.send('Não estou tocando nada no momento.')
+else {
+message.channel.send('Música terminada!').then(player.stop())
+}
+}
+
+exports.config = {
+    name: 'stop',
+    aliases: [],
+    category: 'music'
+}
