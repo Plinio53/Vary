85f5f9bbecac4e0bddeebd0ce88671c968d64460
Pedro <pedropivapedropiva1@outlook.com>
2019-08-03 17:07:18 -0300

    Update StopCommand.js

--- commands/StopCommand.js
+++ commands/StopCommand.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 exports.run = async ({vary, message, args}, t) => {
 
 let player = vary.calls.get(message.guild.id)
-if (!player.player.track) return message.channel.send('Não estou tocando nada no momento.')
+if (!player.player.track || !vary.lavalinkManager.manager.has(message.guild.id)) return message.channel.send('Não estou tocando nada no momento.')
 message.channel.send('Música terminada!').then(player.player.stop())
  
 }
