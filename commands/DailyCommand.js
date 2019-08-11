b156cd9c02e06e16e40a8e1ea02ccb73b8c60244
Diogo912 <gameshakers910@gmail.com>
2019-08-11 12:56:53 +0100

    será q agr vai?

--- commands/DailyCommand.js
+++ commands/DailyCommand.js
@@ -11,15 +11,15 @@ exports.run = ({ vary, message, args }, t) => {
     if (documento) {
       let valor = documento.Equipe ? 1200 : documento.Designer ? 700 : documento.Doador ? 600 : documento.Partner ? 500 : 350
       var tempo = moment.duration.format([moment.duration((parseInt(documento.DailyLastTime) + 86400000) - Date.now())], 'hh:mm:ss')
-
-      if ((parseInt(documento.DailyLastTime) + 86400000) <= (Date.now())) {
+       if ((86400000 / 2) - (Date.now() - user.coinsLastTime) > 0) return message.channel.send(t("commands:daily.alreadyGiven", { time: tempo }))
+//     if ((parseInt(documento.DailyLastTime) + 86400000) <= (Date.now())) {
         documento.Smiles += valor //documento.Smiles + valor
         documento.DailyLastTime = Date.now()
         documento.save()
-        message.channel.send(t('comandos:daily.collectedMoney', { smiles: valor }))
-      } else {
-        message.channel.send(t('comandos:daily.alreadyGiven', { time: tempo }))
-      }
+        message.channel.send(t("comandos:daily.collectedMoney", { smiles: valor }))
+/*      } else {
+        message.channel.send(t("comandos:daily.alreadyGiven", { time: tempo }))
+      } */
     } else {
        message.channel.send("Ocorreu um erro ao executar o comando...")
     }
